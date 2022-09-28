b9a447bde827a40abbd56efb3e026341c756bfc6
mukhamedovislam <bozuy24@gmail.com>
2022-09-28 09:47:36 +0400

    Add FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,29 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
+import java.io.*;
 
 public class FileReader {
+	public Profile getDataFromFile(File file) {
+		Profile profile = new Profile();
+        try (FileInputStream reader = new FileInputStream(file)) {
+            byte[] bytes = new byte[reader.available()];
+            while (reader.read(bytes)>0) {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (byte c : bytes) {
+                    stringBuilder.append((char) c);
+                }
+                String s = stringBuilder.toString();
+                String[] split = s.split("\n");
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
-    }
+                profile.setName(split[0].split(":")[1].trim());
+                profile.setAge(Integer.parseInt(split[1].split(":")[1].trim()));
+                profile.setEmail(split[2].split(":")[1].trim());
+                profile.setPhone(Long.parseLong(split[3].split(":")[1].trim()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		return profile;
+	}
 }
